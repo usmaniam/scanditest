de863a6328fa6869b9f65ffab74fc2669cb3e353
Usman Abubakar <99250944+usmaniam@users.noreply.github.com>
2023-04-08 09:41:10 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,5 +1,11 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: *");
+
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../config/config.php';
 
