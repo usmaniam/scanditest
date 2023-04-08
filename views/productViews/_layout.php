138aa1c4e224dbbb99135a56cf335eef77805dcb
Usman Abubakar <99250944+usmaniam@users.noreply.github.com>
2023-04-08 17:39:28 +0100

    Update _layout.php

--- views/productViews/_layout.php
+++ views/productViews/_layout.php
@@ -10,6 +10,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" />
+    <base href="https://scandi-test.herokuapp.com/">
     <link rel="stylesheet" href="/public/../app.css" />
     <script defer src="/public/../app.js"></script>
     <title>Scandiweb Test Task</title>
